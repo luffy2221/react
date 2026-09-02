cbb47719ca08d2280b349db895fb2012d3e9f8d4
hanish kumawat <gymnasthanish22@gmail.com>
2026-09-02 21:45:04 +0530

    cards_dynamic_branch_02-09-2026

--- src/components/Section1/RightContent.jsx
+++ src/components/Section1/RightContent.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import RightCard from './RightCard'
 
 const RightContent = (props) => {
-  console.log("afjifj", props.users);
-  console.log("4:15 something");
-  
+
   return (
-    <div className='h-full overflow-x-auto p-6 w-2/3 flex flex-nowrap gap-10 rounded-3xl'>
-      < RightCard />
+    <div id='right' className='h-full overflow-x-auto p-6 w-2/3 flex flex-nowrap gap-10 rounded-3xl'>
+      {props.users.map(function(elem ,idx){
+        return <RightCard key={idx} id={idx} color={elem.color} img={elem.img} intro ={elem.intro} tag={elem.tag}/>
+      })}
     </div>
   )
 }
